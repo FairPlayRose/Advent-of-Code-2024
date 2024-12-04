11f320c8daf0f22084b7bcd24b9fb5fb3b011e36
FairPlayRose <frederikp95@gmail.com>
2024-12-04 08:30:43 +0100

    Updated day 3 solutions

--- python/Day 3 - Mull It Over 1.py
+++ python/Day 3 - Mull It Over 1.py
@@ -2,11 +2,10 @@ import re
 
 input = list()
 
-with open("input/day_03.txt", "r") as file:
+with open("input/day_03_test.txt", "r") as file:
     for line in file:
-        result = re.findall(r"(mul\([0-9]{1,3},[0-9]{1,3}\))", line)
-        for mult in result:
-            numbers = re.findall(r"([0-9]{1,3})", mult)
-            input.append(int(numbers[0]) * int(numbers[1]))
+        result = re.findall(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)", line)
+        for a,b in result:
+            input.append(int(a) * int(b))
 
 print(sum(input))
--- python/Day 3 - Mull It Over 2.py
+++ python/Day 3 - Mull It Over 2.py
@@ -2,21 +2,20 @@ import re
 
 input = list()
 
-with open("input/day_03.txt", "r") as file:
+with open("input/day_03_test_2.txt", "r") as file:
     for line in file:
-        result = re.findall(r"(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don't\(\))", line)
-        result = [a+b+c for a,b,c in result]
+        result = re.findall(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do\(\))|(don't\(\))", line)
+        print(result)
 
         do_stuff = True
         for group in result:
-            if group == 'do()':
-                do_stuff = True
-            elif group == "don't()":
-                do_stuff = False
-            elif do_stuff == True:
-                numbers = re.findall(r"([0-9]{1,3})", group)
-                input.append(int(numbers[0]) * int(numbers[1]))
-
+            match group:
+                case (a, b, '', '') if do_stuff == True:
+                    input.append(int(a)*int(b))
+                case ('', '', 'do()', ''):
+                    do_stuff = True
+                case ('', '', '', "don't()"):
+                    do_stuff = False
     
 print(sum(input))
 
