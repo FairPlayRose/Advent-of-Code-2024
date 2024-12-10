54d6c4593755741c0550cab4cbcd7735282ff8f1
FairPlayRose <frederikp95@gmail.com>
2024-12-10 12:41:15 +0100

    attempt 2 at Day 9 part 2 - fail

--- python/Day 9 - Disk Fragmenter 2.py
+++ python/Day 9 - Disk Fragmenter 2.py
@@ -1,48 +1,60 @@
 input = list()
 
-with open("input/day_09_test.txt", "r") as file:
+with open("input/day_09.txt", "r") as file:
     for line in file:
         input.append(line.rstrip())
 
 input = input[0]
 
-input = [int(i) for i in input]
-input = [['data' if index % 2 == 0 else 'break', index//2, num]
-         for index, num in enumerate(input)]
+data = [[int(file), i//2] for i, file in enumerate(input) if i % 2 == 0]
+space = [int(gap) for i, gap in enumerate(input) if i % 2 == 1] + [0]
 
-rev_input = reversed(input)
+data_index = len(data) - 1
+start_space_index = 0
 
-print(input)
+#print(data)
+#print([file[0] for file in data], data_index)
+#print(space, start_space_index)
+#print('')
 
-right_meet_left = False
-left_index = 0
-right_index = 0
+while start_space_index < data_index:
+    space_copy = space.copy()
 
-break_space = list()
-disk_space = 0
+    for i in range(start_space_index, data_index):
 
+        file = data[data_index]
+
+        if file[0] <= space_copy[i]:
+            space = space[0:i] + [0, space[i] - file[0]] + space[i+1:data_index-1] + [file[0] + space[data_index-1] + space[data_index]] + space[data_index+1:]
+            data = data[0:i+1] + [data[data_index]] + data[i+1:data_index] + data[data_index+1:]
+            data_index += 1
+            break
+
+    data_index -= 1
+    for i, gap in enumerate(space):
+        if gap == 0:
+            continue
+        start_space_index = i
+        break
+    
+    #print(data)
+    #print([file[0] for file in data], data_index)
+    #print(space, start_space_index)
+    #print('')
+
+space = [0] + space[:-1]
+
+#print(data)
+#print([file[0] for file in data], data_index)
+#print(space, start_space_index)
+
+position = 0
 check_sum = 0
 
-while right_meet_left == False:
-    #print(left_index, right_index)
-    if left_index + right_index >= len(input):
-        right_meet_left == True
-        break
+for index, [file_space, file_index] in enumerate(data):
+    position += space[index]
+    for i in range(position, position + file_space):
+        check_sum += i*file_index
+    position += file_space
 
-    message, file_index, size = input[left_index]
-    if message == 'data':
-        for disk_index in range(disk_space, disk_space + size):
-            check_sum += disk_index*file_index
-        disk_space += size
-        left_index += 1
-    if message == 'break':
-        break_space.append(size)
-        message2, index2, size2 = rev_input[right_index]
-        if message2 == 'data':
-            for space in break_space:
-                if size2 < space:
-                    pass
-                    
-        
-
-#print(check_sum)
+print(check_sum)
