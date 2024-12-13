0049340bbff625a3459c78d116c95332a6b3d2f7
FairPlayRose <frederikp95@gmail.com>
2024-12-13 15:17:04 +0100

    Day 12 - progress

--- python/Day 12 - Garden Groups 2.py
+++ python/Day 12 - Garden Groups 2.py
@@ -7,20 +7,23 @@ with open("input/day_12_test.txt", "r") as file:
         input.append([x for x in line.rstrip()])
 
 
-def plantAreas(plant: str, pos: tuple[int,int], field: list[str]) -> tuple[int,set[tuple[int,int]],set[tuple[int,int]]]:
+def plantAreas(plant: str, pos: tuple[int,int], field: list[str], direction: str) -> tuple[int,set[tuple[int,int]],set[tuple[int,int]]]:
     pos_y, pos_x = pos
 
     if not pos_y in range(0, len(field)) or not pos_x in range(0, len(field)):
-        return (0,{},{pos}) #area, pos_set, border_set
+        return (0,{},{(pos_y, pos_x, direction)}) #area, pos_set, border_set
     
     if '#' == field[pos_y][pos_x]:
         return (0,{},{}) #area, pos_set, border_set
         
     if plant != field[pos_y][pos_x]:
-        return (0,{},{pos}) #area, pos_set, border_set
+        return (0,{},{(pos_y, pos_x, direction)}) #area, pos_set, border_set
         
 
     new_pos = [(pos_y+dy,pos_x+dx) for dy, dx in [(1,0), (0,1), (-1,0), (0,-1)]]
+
+    new_pos_v = [(pos_y+dy,pos_x+dx) for dy, dx in [(0,1), (0,-1)]]
+    new_pos_h = [(pos_y+dy,pos_x+dx) for dy, dx in [(1,0), (-1,0)]]
 
     field[pos_y][pos_x] = '#'
 
@@ -28,12 +31,17 @@ def plantAreas(plant: str, pos: tuple[int,int], field: list[str]) -> tuple[int,s
     pos_set = {pos}
     border_set = set()
 
-    for new in new_pos:
-        darea, dpos_set, dborder_set = plantAreas(plant, new, field)
+    for new in new_pos_v:
+        darea, dpos_set, dborder_set = plantAreas(plant, new, field, '|')
         area += darea
         pos_set = pos_set.union(dpos_set)
         border_set = border_set.union(dborder_set)
         #print(dborder_set, border_set)
+    for new in new_pos_h:
+        darea, dpos_set, dborder_set = plantAreas(plant, new, field, '-')
+        area += darea
+        pos_set = pos_set.union(dpos_set)
+        border_set = border_set.union(dborder_set)
 
     return (area, pos_set, border_set)
 
@@ -43,20 +51,8 @@ def detect_range(pos: list[tuple[int,int]]):
         j = list(j)
         yield j[0][1], j[-1][1]
 
-area, pos_set, border_set = plantAreas('R', (0,0), input)
+area, pos_set, border_set = plantAreas('R', (0,0), input, '')
 border_list = list(border_set)
 border_list.sort()
 print(border_list)
 
-
-#check_sum = 0
-#
-#for i in range(len(input)):
-#    for j in range(len(input)):
-#        if input[i][j] != '.':
-#            area, border, pos_set, border_set = plantAreas(input[i][j], (i,j), input)
-#            check_sum += area*border
-#            for pos_y, pos_x in pos_set:
-#                input[pos_y][pos_x] = '.'
-#
-#print(check_sum)
